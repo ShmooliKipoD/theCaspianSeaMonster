cdb8774cd56854e8ed174b869d82666648fca7b4
shmuelsCamtek <shmuels@camtek.com>
2025-07-03 08:58:37 +0300

    feat: enhance PlayerSystem with Transform2 component and keyboard input handling

--- src/Systems/PlayerSystem.cs
+++ src/Systems/PlayerSystem.cs
@@ -1,4 +1,7 @@
+using MonoGame.Extended;
+using MonoGame.Extended.Input;
 using theCaspianSeaMonster.Collisions;
+using theCaspianSeaMonster.Entities;
 
 namespace theCaspianSeaMonster.Systems;
 
@@ -7,27 +10,71 @@ class PlayerSystem : EntityProcessingSystem
     private ComponentMapper<Body> _bodyMapper;
     private ComponentMapper<Sprite> _spriteMapper;
     private ComponentMapper<AnimatedSprite> _animatedSpriteMapper;
+    private ComponentMapper<Transform2> _transformMapper;
 
     public PlayerSystem()
         : base(
             Aspect.All(
                 typeof(Body)
+                , typeof(Transform2)
                 , typeof(Sprite)
                 , typeof(AnimatedSprite)
+                , typeof(Player)
                 )
-            ) { }
+            )
+    { }
 
     public override void Initialize(IComponentMapperService mapperService)
     {
         _bodyMapper = mapperService.GetMapper<Body>();
-        _spriteMapper = mapperService.GetMapper<Sprite>();        
+        _spriteMapper = mapperService.GetMapper<Sprite>();
         _animatedSpriteMapper = mapperService.GetMapper<AnimatedSprite>();
+        _transformMapper = mapperService.GetMapper<Transform2>();
     }
 
     public override void Process(GameTime gameTime, int entityId)
     {
-        
+
         var animaton = _animatedSpriteMapper.Get(entityId);
+        var body = _bodyMapper.Get(entityId);
+        var transform = _transformMapper.Get(entityId);
+        // var sprite = _spriteMapper.Get(entityId);
+
+        KeyboardStateExtended keyboardState = KeyboardExtended.GetState();
+
         animaton.SetAnimation("Forward");
+
+        if (keyboardState.IsKeyDown(Keys.Right)
+            || keyboardState.IsKeyDown(Keys.D)
+            )
+        {
+            animaton.SetAnimation("Right");
+            body.Velocity.X += 150;
+        }
+
+        if (keyboardState.IsKeyDown(Keys.Left)
+            || keyboardState.IsKeyDown(Keys.A)
+            )
+        {
+            animaton.SetAnimation("Left");
+            body.Velocity.X -= 150;
+        }
+
+        if (keyboardState.IsKeyDown(Keys.Up)
+            || keyboardState.IsKeyDown(Keys.W)
+            )
+        {
+            body.Velocity.Y -= 150;
+        }
+
+        if (keyboardState.IsKeyDown(Keys.Down)
+            || keyboardState.IsKeyDown(Keys.S)
+            )
+        {
+            body.Velocity.Y += 150;
+        }
+
+        // transform.Position += body.Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
     }
 }
