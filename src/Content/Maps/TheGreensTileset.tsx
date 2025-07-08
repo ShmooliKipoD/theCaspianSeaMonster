e18ec3bedc71fb74b7c0308bc22dfe59d2a293bf
shmuelsCamtek <shmuels@camtek.com>
2025-07-08 09:33:26 +0300

    Add GreenLakeBackgroundSystem class for rendering background entities
    - Introduced a new class GreenLakeBackgroundSystem that extends EntityDrawSystem.
    - Set up the constructor to use an aspect that includes all entities.
    - Placeholder methods for Initialize and Draw with NotImplementedException.

---
+++ src/Content/Maps/TheGreensTileset.tsx
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="TheGreensTileset" tilewidth="32" tileheight="32" tilecount="27" columns="9">
+ <image source="TheGreens.png" width="288" height="96"/>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="100"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="17" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ src/Systems/GreenLakeBackgoundSystem.cs
@@ -0,0 +1,25 @@
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+
+namespace theCaspianSeaMonster.Systems;
+
+class GreenLakeBackgoundSystem : EntityDrawSystem
+{
+    public GreenLakeBackgoundSystem()
+        : base(
+            Aspect.All()
+        )
+    { }
+
+    public override void Initialize(IComponentMapperService mapperService)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Draw(GameTime gameTime)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
